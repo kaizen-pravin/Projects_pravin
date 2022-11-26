a9dda308e7c45361d475291ddad105c981a1ce9b
Kaizen-pravin <pravinpk1601@gmail.com>
2022-11-26 15:31:34 +0530

    First Commit

---
+++ src/main/java/Pravin_project1/sampleproject/App.java
@@ -0,0 +1,13 @@
+package Pravin_project1.sampleproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello world!" );
+    }
+}
